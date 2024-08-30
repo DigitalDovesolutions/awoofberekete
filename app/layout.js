65fffc21ebe72f860d34bd38a6158b98b2f01699
David idornigie <idornigiedavid1@gmail.com>
2024-08-30 19:06:37 +0100

    Removed unused imports

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import SideNav from "@/components/SideNav";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
