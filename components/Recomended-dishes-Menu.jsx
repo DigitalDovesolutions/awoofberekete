d1f87e1e7ebae059c33df9edd9861f8aea5bf5ef
David idornigie <idornigiedavid1@gmail.com>
2024-09-04 17:11:55 +0100

    Refactor RecomendedDishesMenu and Services components, update CSS styles

--- components/Recomended-dishes-Menu.jsx
+++ components/Recomended-dishes-Menu.jsx
@@ -9,9 +9,15 @@ const RecomendedDishesMenu = ({ items, activeFilter }) => {
       <div className="grid gap-[34px] min-[476px]:gap-y-[30px] lg:gap-[29px] lg:gap-y-20 md:gap-y-20 max-md:gap-y-20 max-[476px]:grid-cols-1 min-[476px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {items?.map((item, idx) => (
           <div key={idx} className="max-[767px]:w-full">
-            <div className={`meal-card max-[767px]:w-full relative ${item.category} ${
-                activeFilter !== "all" && item.category !== activeFilter ? "hidden" : "block"
-              } p-[20px_10px_10px_10px] bg-white shadow-lg rounded-[0px_50px_0px_50px]`}>
+            <div
+              className={`meal-card max-[767px]:w-full relative ${
+                item.category
+              } ${
+                activeFilter !== "all" && item.category !== activeFilter
+                  ? "hidden"
+                  : "block"
+              } p-[20px_10px_10px_10px] bg-white shadow-lg rounded-[0px_50px_0px_50px]`}
+            >
               <Image
                 src={item.image}
                 alt={`Image of ${item.mealName}`}
@@ -22,19 +28,32 @@ const RecomendedDishesMenu = ({ items, activeFilter }) => {
               />
               <div className="p-4 min-[767px]:pt-24 min-[476px]:pt-[170px]">
                 <h4 className="text-lg font-semibold mb-2 line-clamp-1">
-                    <Link href="/">
-                        {item.mealName}
-                    </Link>
+                  <Link href="/">{item.mealName}</Link>
                 </h4>
                 <div className="flex justify-between items-center">
                   <div className="stars flex">
                     {[...Array(5)].map((_, starIdx) => {
                       if (starIdx < item.rating) {
-                        return <FaStar key={starIdx} className="text-yellow-500 mr-1" />;
+                        return (
+                          <FaStar
+                            key={starIdx}
+                            className="text-yellow-500 mr-1"
+                          />
+                        );
                       } else if (starIdx < item.rating + 0.5) {
-                        return <FaStarHalfAlt key={starIdx} className="text-yellow-500 mr-1" />;
+                        return (
+                          <FaStarHalfAlt
+                            key={starIdx}
+                            className="text-yellow-500 mr-1"
+                          />
+                        );
                       } else {
-                        return <FaRegStar key={starIdx} className="text-yellow-500 mr-1" />;
+                        return (
+                          <FaRegStar
+                            key={starIdx}
+                            className="text-yellow-500 mr-1"
+                          />
+                        );
                       }
                     })}
                     <span className="text-sm font-semibold text-primary opacity-75">
@@ -49,16 +68,18 @@ const RecomendedDishesMenu = ({ items, activeFilter }) => {
                 </div>
               </div>
 
-              <div className="flex flex-nowrap justify-between items-center max-[767px]:mt-3 max-[767px]:mt-1 px-4">
+              <div className="flex flex-nowrap justify-between items-center max-[767px]:mt-1 px-4">
                 <div className="price">
-                  <span className="font-semibold text-[15px]">N{parseFloat(item.price).toFixed(2)}</span>
+                  <span className="font-semibold text-[15px]">
+                    N{parseFloat(item.price).toFixed(2)}
+                  </span>
                 </div>
 
                 <button
-                    className="bg-danger text-white py-2 px-2 text-sm rounded-full"
-                    onClick={() => router.push("/menu")}
-                    >
-                    Buy Now
+                  className="bg-danger text-white py-2 px-2 text-sm rounded-full"
+                  onClick={() => router.push("/menu")}
+                >
+                  Buy Now
                 </button>
               </div>
             </div>
--- components/sections/Services.jsx
+++ components/sections/Services.jsx
@@ -2,8 +2,8 @@ import ServicesItems from "../ServicesItems"
 
 const Services = () => {
   return (
-    <section id="services" className="relative max-md:bg-secondary max-[876px]:py-16 min-[878px]:p-24 lg:p-0 z-40">
-      <div className="container">
+    <section id="services" className="max-md:bg-secondary max-[876px]:py-16 min-[878px]:p-24 lg:p-0">
+      <div className="container relative overflow-visible z-40">
         <ServicesItems/>
       </div>
     </section>
